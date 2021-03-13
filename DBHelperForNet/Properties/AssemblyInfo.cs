3da1fa65316e26c8109741b7b3694c60853d199a
Leo W <leowidodo037@gmail.com>
2021-03-13 11:04:01 +0700

    1

--- DBHelperForNet/Properties/AssemblyInfo.cs
+++ DBHelperForNet/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DBHelperForNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("DB Connection for ado.net")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DBHelperForNet")]
